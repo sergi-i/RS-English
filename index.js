f1ee5e2330f33cdb415174685145807966801cac
Irina Sergienko <90563560+sergi-i@users.noreply.github.com>
2022-08-21 21:47:45 +0300

    Add files via upload
    added fixed menu

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const heading = document.getElementById('hello')
+const heading2 = document.querySelector('#sub-hello')
+console.dir(heading2)
+const heading3 = document.querySelector('#sub-hello2')
 
 setTimeout(() => {
- addStyleTo(heading)
+ addStyleTo(heading, 'RS English', '#EC1616', '#D9D9D9', '2.5em', '2em')
 }, 1500)
 
-function addStyleTo(node) {
- node.style.color = '#EC1616'
- node.style.backgroundColor = '#D9D9D9'
+setTimeout(() => {
+ addStyleTo(heading3, 'Самое важное в изучении английского – понять ЗАЧЕМ Вам это надо и надо ли это вообще...', '#3D4852')
+}, 3000)
+
+setTimeout(() => {
+ addStyleTo(heading2, 'На этом сайте собраны лекции и полезные материалы с уроков английского языка для сотрудников ROCKET SCIENCE', '#fff', '#3D4852', '2rem')
+}, 4500)
+
+function addStyleTo(node, text, color = '#EC1616', backgroundColor = '#D9D9D9', padding = '2rem', fontSize = '1em') {
+ node.textContent = text
+ node.style.color = color
+ node.style.backgroundColor = backgroundColor
+ node.style.padding = padding 
+ node.style.fontSize = fontSize
 }
+
 
 
 
